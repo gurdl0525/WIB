2d3d060394ffbceb5886c158bcbcaeca23109133
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-08-11 00:46:31 +0900

    ⚡️ :: 여러 분야 검색 가능하도록 성능개선

--- app/format/format.py
+++ app/format/format.py
@@ -2,73 +2,75 @@ from collections import Counter
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from PIL import Image
+from fastapi import HTTPException
+from wordcloud import WordCloud, STOPWORDS
+
 from .model.data import FrameWork, Language
-from ..db.base import select_data_from_table, find_all_tech_by_notice_id
-from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+from ..db.base import find_all_tech_by_occ
 
 frameworkList, languageList = FrameWork.list, Language.list
 
 
-def data_format():
-    techList = select_data_from_table('tech')
+def data_format_p(tag: str, occ: str):
+    techList = find_all_tech_by_occ(occ)
 
     if techList is None:
-        return {'message': '먼저 테크리스트를 업데이트 해주세요'}
+        raise HTTPException(status_code=400, detail={'message': '먼저 테크리스트를 업데이트 해주세요'})
 
-    framework, language = [], []
+    result = []
 
-    for v in techList:
+    if tag == 'language':
 
-        vText = v['text']
-        vNotice = v['notice_id']
+        for v in techList:
 
-        if vText in frameworkList:
-            if not isinstance(frameworkList[vText], list):
-                if vText == 'Spring' or vText == 'Spring Boot':
-                    framework.append(f'{frameworkList[vText]}-Spring/SpringBoot')
-                elif vText == 'DRF(Django REST framework)':
-                    framework.append(f'{frameworkList[vText]}-Django')
+            if v.text in languageList:
+                result.append(v.text)
+
+    elif tag == 'framework':
+
+        for v in techList:
+
+            if v.text in frameworkList:
+
+                result.append(v.text if v.text != 'DRF(Django REST framework)' else 'Django')
+
+    elif tag == 'compound':
+
+        l, n = [], 0
+
+        for v in techList:
+
+            if n != v.id:
+                n = v.id
+                l.clear()
+
+            txt = v.text
+
+            if txt in frameworkList:
+
+                if isinstance(frameworkList[txt], list):
+                    for lang in list(set(l).intersection(frameworkList[txt])):
+                        result.append(lang + '-' + txt if txt != 'DRF(Django REST framework)' else 'Django')
                 else:
-                    framework.append(f'{frameworkList[vText]}-{vText}')
-            else:
-                finded = find_all_tech_by_notice_id(vNotice)
-                for k in frameworkList[vText]:
-                    for i in finded:
-                        if i.text == k:
-                            if vText == 'Spring' or vText == 'Spring Boot':
-                                framework.append(f'{k}-Spring/SpringBoot')
-                            elif vText == 'DRF(Django REST framework)':
-                                framework.append(f'{k}-Django')
-                            else:
-                                framework.append(f'{k}-{vText}')
+                    result.append(frameworkList[txt] + '-' + txt if txt != 'DRF(Django REST framework)' else 'Django')
 
-        elif vText in languageList:
-            language.append(vText)
+            elif txt in languageList:
+                l.append(txt)
+    else:
+        raise HTTPException(status_code=400, detail={'message': 'Invalid Tag Exception'})
 
-    return {'frameworkCountList': dict(Counter(framework)), 'languageCountList': dict(Counter(language))}
+    return dict(Counter(result))
 
 
-def all_format():
-    techList = select_data_from_table('tech')
+def show_data_format(typ: str, tag: str, occ: str):
 
-    if techList is None:
-        return {'message': '먼저 테크리스트를 업데이트 해주세요'}
+    if typ != 'pie' and typ != 'bar':
+        raise HTTPException(status_code=400, detail={'message': 'Invalid Type Exception'})
 
-    return {'allCountList': Counter(v['text'] for v in techList)}
+    df = data_format_p(tag, occ)
 
-
-def show_data_format(tag: str, typ: str):
-    if (tag != 'framework' and tag != 'language') or (typ != 'pie' and typ != 'bar'):
-        return {'message': 'Invalid Tag Exception'}
-
-    df = data_format()
-
-    if df == {'message': '먼저 테크리스트를 업데이트 해주세요'}:
-        return df
-
-    lists, value, label = df[f'{tag}CountList'], [], []
+    lists, value, label = df, [], []
 
     onePercentage, etc = get_one_percentage(lists), 0
 
@@ -83,21 +85,18 @@ def show_data_format(tag: str, typ: str):
         label.append('기타')
         value.append(etc)
 
-    create_image(value, label, f'{tag}_count_{typ}', typ)
+    create_image(value, label, f'{tag}_count_{typ}', typ, occ)
 
-    return f'app/format/img/{tag}_count_{typ}.png'
+    return f'app/format/img/{occ}/{tag}_count_{typ}.png'
 
 
-def create_image(value, label, file_name, typ: str):
+def create_image(value: list, label: list, file_name: str, typ: str, occ :str):
     rainbow = ['red', 'gold', 'limegreen', 'mediumpurple', 'skyblue', 'dodgerblue', 'darkviolet']
     if typ == 'pie':
         plt.rc('font', family='Apple SD Gothic Neo', size=7)
         plt.rcParams['text.color'] = "Black"
         plt.rcParams['axes.unicode_minus'] = False
-        if file_name == 'framework_count_pie':
-            plt.title('Framework Count Pie Chart', fontdict={'fontsize': 20})
-        else:
-            plt.title('Language Count Pie Chart', fontdict={'fontsize': 20})
+        plt.title(file_name.title().replace('_', ' ') + ' Chart', fontdict={'fontsize': 20})
         size = len(label)
         colors = []
         for i in range(size):
@@ -112,27 +111,29 @@ def create_image(value, label, file_name, typ: str):
             colors=colors
         )
         plt.tight_layout()
-        plt.savefig(f'app/format/img/{file_name}.png', format='png', dpi=200)
+        plt.savefig(f'app/format/img/{occ}/{file_name}.png', format='png', dpi=200)
     else:
-        if file_name == 'framework_count_bar':
-            plt.title('Framework Count Bar Graph', fontdict={'fontsize': 20})
-            plt.xlabel('Language-Framework',
-                       fontdict={'fontsize': 10, 'family': 'Apple SD Gothic Neo', 'color': 'black'})
-        else:
-            plt.title('Language Count Bar Graph', fontdict={'fontsize': 20})
-            plt.xlabel('Languages', fontdict={'fontsize': 10, 'family': 'Apple SD Gothic Neo', 'color': 'black'})
+        plt.title(file_name.title().replace('_', ' ') + ' Bar Graph', fontdict={'fontsize': 20})
+        plt.xlabel(
+            xlabel='languages' if file_name == 'language_count_bar' else 'Frameworks' if file_name == 'framework_count_bar' else 'Compounds',
+            fontdict={
+                'fontsize': 10,
+                'family': 'Apple SD Gothic Neo',
+                'color': 'black'
+            }
+        )
         plt.rc('axes', unicode_minus=False)
         size = range(len(label))
         colors = []
         for i in size:
             colors.append(rainbow[i % 7])
-        bars = plt.bar(size, value, color=colors)
+        bars = plt.bar(size, value, color=colors, width=0.5)
         plt.legend(handles=bars, labels=label, prop={'family': 'Apple SD Gothic Neo'})
         plt.grid(True, axis='y', alpha=0.5, color='gray', linestyle='--')
         plt.xticks(size, label, fontdict={'fontsize': 4, 'family': 'Apple SD Gothic Neo', 'color': 'black'})
         plt.ylabel('Count', fontdict={'fontsize': 10, 'family': 'Apple SD Gothic Neo', 'color': 'black'})
         plt.tight_layout()
-        plt.savefig(f'app/format/img/{file_name}.png', format='png', dpi=200)
+        plt.savefig(f'app/format/img/{occ}/{file_name}.png', format='png', dpi=200)
     plt.cla()
     plt.clf()
 
@@ -145,13 +146,13 @@ def get_one_percentage(lists: dict):
     return result / 50
 
 
-def get_cloud_word():
-    techList = select_data_from_table('tech')
+def get_cloud_word(occ: str):
+    techList = find_all_tech_by_occ(occ)
 
     if techList is None:
-        return {'message': '먼저 테크리스트를 업데이트 해주세요'}
+        raise HTTPException(status_code=400, detail={'message': '먼저 테크리스트를 업데이트 해주세요'})
 
-    techList = [v['text'] for v in techList]
+    techList = [v.text for v in techList]
 
     bulb = np.array(Image.open('app/format/img/bulb.png'))
 
@@ -159,8 +160,16 @@ def get_cloud_word():
     stopwords.add("bulb")
     stopwords.add("tech")
 
-    wc = WordCloud(background_color="white", max_words=2000, mask=bulb, width=5000, height=5000,
-                   stopwords=stopwords, contour_width=1, contour_color='steelblue')
+    wc = WordCloud(
+        background_color="white",
+        max_words=2000,
+        mask=bulb,
+        width=5000,
+        height=5000,
+        stopwords=stopwords,
+        contour_width=1,
+        contour_color='steelblue'
+    )
 
     tech_str = ''
 
@@ -171,7 +180,7 @@ def get_cloud_word():
 
     plt.imshow(wc, cmap=plt.cm.gray, interpolation='bilinear')
     plt.axis("off")
-    plt.tight_layout
-    plt.savefig('app/format/img/cloudword.png', format='png', dpi=200)
+    plt.tight_layout()
+    plt.savefig(f'app/format/img/{occ}/cloudword.png', format='png', dpi=300)
 
-    return 'app/format/img/cloudword.png'
+    return f'app/format/img/{occ}/cloudword.png'
