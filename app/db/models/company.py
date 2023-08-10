bb36586371fd25471ef210f8ffbb4719879b07d1
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-08-11 00:42:28 +0900

    🔥 :: Company Entity

--- app/db/models/company.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, VARCHAR
-from sqlalchemy.orm import relationship
-from ..base_class import Base
-
-
-class Company(Base):
-    __tablename__ = "company"
-    id = Column(VARCHAR(200), primary_key=True)
-    tech = relationship("Tech", lazy=True)
