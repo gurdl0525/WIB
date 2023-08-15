dab8cda411f1c0fd929703e4d61ef555a7b9296b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-08-15 22:45:56 +0900

    ✨ :: wanted crawling

---
+++ app/crawler/wanted.py
@@ -0,0 +1,44 @@
+import datetime
+import json
+
+import requests
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+
+from app.core.config import OccupationalW
+from app.db.base import create_tech
+
+
+def crawl_wanted(occ: str):
+    for o in OccupationalW:
+        if o.name == occ:
+            occ = o
+            break
+
+    if isinstance(occ, str):
+        raise HTTPException(status_code=400, detail={'message': '잘못된 분야'})
+
+    n_id_list, offset = [], 0
+
+    while True:
+
+        req = requests.get(
+            fr'https://www.wanted.co.kr/api/v4/jobs?{int(datetime.datetime.now().timestamp())}&country=kr&tag_type_ids={occ.value}&job_sort=company.response_rate_order&locations=all&years=-1&limit=100&offset={offset}')
+
+        try:
+            n_id_list += [i['id'] for i in json.loads(req.text)['data']]
+
+        except TypeError:
+            break
+
+        offset += 100
+
+    for n_id in set(n_id_list):
+
+        request = requests.get(fr'https://www.wanted.co.kr/api/v4/jobs/{n_id}?{int(datetime.datetime.now().timestamp())}')
+
+        try:
+            for tag in json.loads(request.text)['job']['skill_tags']:
+                create_tech(n_id, tag['title'], "WANTED", occ.name)
+        except IntegrityError:
+            continue
