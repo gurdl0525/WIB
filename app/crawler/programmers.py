d4c0b57acd82098def4eebeda2d6f99c9b7af688
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-08-15 22:48:33 +0900

    ♻️ :: 함수 병합

--- app/crawler/programmers.py
+++ app/crawler/programmers.py
@@ -4,50 +4,48 @@ import requests
 from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 
+from app.core.config import OccupationalP
 from app.db.base import create_tech
-from app.db.models.tech import OccupationalP
 
 
 def crawl_programmers(occ: str):
-    tag: int = None
 
-    for e in OccupationalP:
-        if e.name == occ:
-            tag = e.value
+    for o in OccupationalP:
+        if o.name == occ:
+            occ = o
             break
 
-    if tag is None:
+    if isinstance(occ, str):
         raise HTTPException(status_code=400, detail={'message': '잘못된 분야'})
 
     totalPage = int(dict(json.loads(requests.get(
-        url=rf'https://career.programmers.co.kr/api/job_positions?order=recent&page=1&job_category_ids[]={tag}'
+        url=rf'https://career.programmers.co.kr/api/job_positions?order=recent&page=1&job_category_ids[]={occ.value}'
     ).text))['totalPages'])
 
     for i in range(totalPage + 1)[1::]:
 
         response = dict(json.loads(requests.get(
-            url=rf'https://career.programmers.co.kr/api/job_positions?order=recent&page={i}&job_category_ids[]={tag}'
+            url=rf'https://career.programmers.co.kr/api/job_positions?order=recent&page={i}&job_category_ids[]={occ.value}'
         ).text))
 
-        for j in response['jobPositions']:
+        for n in response['jobPositions']:
+
+            n_id = int(n['id'])
+
+            response = requests.get(url=f'https://career.programmers.co.kr/api/job_positions/{n_id}')
+
+            if response.status_code == 401:
+                continue
+
             try:
-                update_tech(int(j['id']), occ)
+                response = dict(json.loads(response.text))['jobPosition']
+            except KeyError:
+                continue
+
+            try:
+                for j in response['technicalTags']:
+                    create_tech(n_id, j['name'], "PROGRAMMERS", occ.name)
+
             except IntegrityError:
                 continue
 
-    return
-
-
-def update_tech(n_id: str or int, occ: str):
-    response = requests.get(url=f'https://career.programmers.co.kr/api/job_positions/{n_id}')
-
-    if response.status_code == 401:
-        return
-
-    try:
-        response = dict(json.loads(response.text))['jobPosition']
-    except KeyError:
-        return
-
-    for j in response['technicalTags']:
-        create_tech(n_id, j['name'], "PROGRAMMERS", occ)
